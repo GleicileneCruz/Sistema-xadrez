b7271274df6800ca35e18bc45beef8af4b2d6d68
Gleicilene <1410522@sga.pucminas.br>
2024-02-22 11:37:36 -0300

    class: Position

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
+
+        Position position = new Position(1, 2);
+        System.out.println(position);
 
 
     }
