72fbb2aa56bd7ef52bfecd07cdb7fe64114a3ff3
Gleicilene <1410522@sga.pucminas.br>
2024-02-23 15:26:22 -0300

    Criação das exceptions e das peças

--- src/main/java/chess/ChessPiece.java
+++ src/main/java/chess/ChessPiece.java
@@ -3,7 +3,6 @@ package chess;
 import boardgame.Board;
 import boardgame.Piece;
 public class ChessPiece extends Piece {
-
     private Color color;
     public ChessPiece(Board board, Color color) {
         super(board);
