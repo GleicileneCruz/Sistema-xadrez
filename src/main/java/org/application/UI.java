e13ad93ae992fe25c6aa145fc49bfa034dd01f4b
Gleicilene <1410522@sga.pucminas.br>
2024-02-28 14:41:16 -0300

    Trocando de jogador a cada turno

--- src/main/java/org/application/UI.java
+++ src/main/java/org/application/UI.java
@@ -1,5 +1,6 @@
 package org.application;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -29,6 +30,13 @@ public class UI {
        catch(RuntimeException e){
             throw new InputMismatchException("Erro lendo a posição do xadrez. Valores válidos de a1 até h8");
        }
+    }
+
+    public static void printMatch(ChessMatch chessMatch){
+        printBoard(chessMatch.getPiece());
+        System.out.println();
+        System.out.println("Turno : " + chessMatch.getTurn());
+        System.out.println("Esperando jogador : " + chessMatch.getCurrentPlayer());
     }
 
     public static void printBoard(ChessPiece[][] pieces){
